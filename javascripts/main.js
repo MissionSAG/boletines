a56a9ef380ff8dc076bde683c1adf1e125cead49
Xanthia <grusantiago@gmail.com>
2022-12-09 11:08:47 -0300

    Eventos

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,4 @@
+/*
 console.log("HOLA MUNDIJIRIJILLO.");
 
 
@@ -7,3 +8,21 @@ contenedor.forEach(function(link){
     console.log(link);
 })
 
+
+let celdas = document.querySelectorAll("td");
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){console.log("CLICK")})
+});
+- Obtener elementos de la clase .close
+- Recorrerlos
+- Agregar un evento click a cada uno de ellos.
+*/
+
+let links = document.querySelectorAll(".close");
+links.forEach(
+    function(link){
+        link.addEventListener("click",function(){
+            console.log("lalal");
+        })    
+    }
+)
