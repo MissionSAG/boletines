76e04c2772d5e27293af9a2a2de92f782d7c71f8
Xanthia <grusantiago@gmail.com>
2022-12-11 19:09:26 -0300

    final

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,8 +21,27 @@ celdas.forEach(function(td){
 let links = document.querySelectorAll(".close");
 links.forEach(
     function(link){
-        link.addEventListener("click",function(){
-            console.log("lalal");
+        link.addEventListener("click",
+        function(ev){
+            ev.preventDefault();
+            let content = document.querySelector('.content')
+                content.classList.remove("fadeInDown");
+                content.classList.remove("animated");
+
+                content.classList.add("fadeOutUp");
+                content.classList.add("animated");
+
+                setTimeout(function(){
+                    location.href = "/";
+                },1000);
+                
+            return false;
         })    
     }
-)
+);
+
+/*let iconos = document.querySelectorAll("i");
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star"); 
+
+})*/
